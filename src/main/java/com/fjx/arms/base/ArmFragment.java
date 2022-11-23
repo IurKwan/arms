04e482bc30685fb8b10943427ef58b12ef8a39c3
IurKwan <guanzhirui@gmail.com>
2022-11-23 17:59:54 +0800

    feat: 支持fragment懒加载

--- src/main/java/com/fjx/arms/base/ArmFragment.java
+++ src/main/java/com/fjx/arms/base/ArmFragment.java
@@ -1,11 +1,18 @@
 package com.fjx.arms.base;
 
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.View;
+
 import androidx.annotation.ContentView;
 import androidx.annotation.LayoutRes;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Lifecycle;
 
+import com.blankj.utilcode.util.ThreadUtils;
 import com.fjx.arms.base.delegate.IFragment;
 import com.fjx.arms.di.component.AppComponent;
 import com.fjx.arms.integration.cache.Cache;
@@ -27,7 +34,12 @@ import io.reactivex.rxjava3.subjects.Subject;
 public abstract class ArmFragment<P extends IPresenter> extends Fragment implements IFragment, FragmentLifecycleable {
 
     private final BehaviorSubject<FragmentEvent> mLifecycleSubject = BehaviorSubject.create();
+
     private Cache<String, Object> mCache;
+
+    private boolean isFirst = true;
+
+    private final Handler mHandler = new Handler(Looper.myLooper());
 
     /**
      * 如果当前页面逻辑简单, Presenter 可以为 null
@@ -70,6 +82,36 @@ public abstract class ArmFragment<P extends IPresenter> extends Fragment impleme
     }
 
     @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        isFirst = true;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        onVisible();
+    }
+
+    private void onVisible() {
+        if (getLifecycle().getCurrentState() == Lifecycle.State.STARTED && isFirst) {
+            // 懒加载
+            mHandler.postDelayed(() -> {
+                lazyLoadData();
+                isFirst = false;
+            }, lazyLoadTime());
+        }
+    }
+
+    public void lazyLoadData() {
+
+    }
+
+    public long lazyLoadTime() {
+        return 300;
+    }
+
+    @Override
     public void setData(@Nullable Object data) {
 
     }
@@ -82,6 +124,7 @@ public abstract class ArmFragment<P extends IPresenter> extends Fragment impleme
             mPresenter.onDestroy();
         }
         this.mPresenter = null;
+        mHandler.removeCallbacksAndMessages(null);
     }
 
 }
